a5377d3c0cf0a387dc2f14e12a8269a33b2179c0
Kbgeptupsi <cristianalarcon109@gmail.com>
2023-04-03 14:10:00 -0500

    Conected devices bluetooh and create logo app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CenterPhobias
 Center Phobias Using Pyhton
+
+
+
+### Instalamos bibliotecas necesarias
+* pip install pybluez
+   ** es una biblioteca que se usa para conectar el bluethooth
+   *pip install git+https://github.com/pybluez/pybluez.git#egg=pybluez **Esta fue la que me funciono.**
+
---
+++ main.py
@@ -0,0 +1,23 @@
+import bluetooth
+
+# Busca dispositivos Bluetooth cercanos
+devices = bluetooth.discover_devices()
+
+# Imprime una lista de los dispositivos encontrados
+for device in devices:
+    print(device)
+
+# Al parecer, el dispositivo que buscas está en la posición 0 de la lista de dispositivos encontrados
+# Tambien puedes buscar el dispositivo por su nombre o dirección MAC (Bluetooth)
+
+
+# Conecta al dispositivo deseado
+address = "24:16:03:00:80:8D" # Reemplaza esto con la dirección Bluetooth de tu smartwatch
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+sock.connect((address, 1))
+
+# Lee los datos enviados a través de la conexión Bluetooth
+while True:
+    data = sock.recv(1024)
+    print("Conectado smarthwatch 7 Pro")
+    print(data)
